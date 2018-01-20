903080abdf1039f2e2c2defedfb86463a5fb511c
dzsoni <dzsoni@freemail.hu>
2018-01-20 11:46:37 +0100

    FOSC=20Mhz

--- include/definitions.h
+++ include/definitions.h
@@ -2,7 +2,7 @@
 #define __definitions_h__
 
 //--------------------DEFINITIONS------------------------------------------
-FOSC            EQU    12000000     //Fosc=12MHz
+FOSC            EQU    20000000d     //Fosc=20MHz
 
 DHT_PORT        EQU    P3          //DHT Port
 
@@ -13,12 +13,13 @@ KI_FAN_DHT      EQU    BENT_DHT+3  //DHT a KI ventillátornál
 
 
 
-/*--------------18 us ot eloallító konstansérték---------------------------
+/*--------------18 ms ot eloallító konstansérték---------------------------
 Ha FOSC= 20000000 (20MHz),akkor 0.6 us-on ként lép a számláló
 1/(FOSC/12)=0.6us 
 X18=18000/0.6=30000  biztonsági ráhagyással 34000 (~20.4ms)
 ....................................*/
-        X18         EQU 34000
+       
+       X18          EQU   34000d
 
 /*-------------A bit értékét eldönto idotartam-----------------------------
 A bit '0' ha 50us-nál kisebb,'1' ha 50us-nál nagyobb idot mértünk
